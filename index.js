06964db42522846b63c7dd55e37cdca3879fd044
Artiom Tesevich <qpckifqp@gmail.com>
2023-07-22 19:19:17 +0300

    mk2

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 let userName = prompt ('Please, enter your name', 'User');
+let UserChoice = '';
+let gameArray = ['rock', 'scissors', 'paper']; 
+let levay = 0;
+let levayDva = 0;
 
 alert (`Hello ${userName}`);
 
-function qwe() {
-  return prompt ('Rock, Scissors, Paper... Please make your move');
-}
-
-
-let i = 0;
-
-function tif () {
-  while (i < 1) {
-    switch (qwe()) {
-      case 'rock':
-        alert ('Molodec');
-        i++;
-        return 'rock';
-      case 'scissors':
-        alert ('Molodec2');
-        i++;
-        return 'scissors';
-      default:
-        alert ('NE Molodec');
-    }
+function primeUserChoice () {
+  UserChoice = prompt ('Rock, Scissors, Paper... Please make your move');
+  switch (UserChoice.toLowerCase()) {
+    case 'rock':
+      return UserChoice = 'rock';
+    case UserChoice = 'scissors':
+      return 'scissors';
+    case UserChoice = 'paper':
+      return 'paper';
+    default:
+      alert ('NE Molodec');
   }
-}
-
-// alert (tif())
-
-let gameArray = ['rock', 'scissors', 'paper']; 
+} 
 
 function randomNumber (numMin, numMax) {
   return Math.floor(Math.random() * (numMax - numMin) + numMin)
 }
 
-let com = gameArray [randomNumber (0, 3)];
-
-alert (com + ' выбрал комп')
-// alert (tif() + ' выбрал я')
-
-if (tif() === 'rock' && com === 'scissors' || tif() === 'scissors' && com === 'paper' || tif() === 'paper' && com === 'rock') {
-  alert ("Ты победил!!!")
-} else {
-  alert ("ты проиграл!!!")
+function game () {
+  (primeUserChoice ());
+  machineChoise = gameArray [randomNumber (0, 3)];
+  alert (`Computer move is: ${machineChoise}`)
+  
+  if (UserChoice === 'rock' && machineChoise === 'scissors' || UserChoice === 'scissors' && machineChoise === 'paper' || UserChoice === 'paper' && machineChoise === 'rock') {
+    levay++;  
+  } else {  
+    levayDva++;
+  }
 }
 
-
+for (levay; levay < 3;){ 
+  game();
+  alert (`Ball User ${levay}` + ` Ball PC ${levayDva}`);
+}
