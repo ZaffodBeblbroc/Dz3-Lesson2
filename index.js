1433fecd4983ef0d3fd856ae55ac1ae6afb12cf8
Artiom Tesevich <qpckifqp@gmail.com>
2023-07-23 08:25:52 +0300

    mk4

--- index.js
+++ index.js
@@ -1,42 +1,61 @@
 let userName = prompt ('Please, enter your name', 'User');
-let UserChoice = '';
+let userChoice = '';
 let gameArray = ['rock', 'scissors', 'paper']; 
-let levay = 0;
-let levayDva = 0;
+let userScore = 0;
+let computerScore = 0;
 
-alert (`Hello ${userName}`);
 
 function primeUserChoice () {
-  UserChoice = prompt ('Rock, Scissors, Paper... Please make your move');
-  switch (UserChoice.toLowerCase()) {
-    case 'rock':
-      return UserChoice = 'rock';
-    case UserChoice = 'scissors':
-      return 'scissors';
-    case UserChoice = 'paper':
-      return 'paper';
-    default:
-      alert ('NE Molodec');
+  userChoice = prompt ('Rock, Scissors, Paper... Please make your move');
+  switch (userChoice.toLowerCase()) {
+  case 'rock':
+    return userChoice = 'rock';
+  case userChoice = 'scissors':
+    return 'scissors';
+  case userChoice = 'paper':
+    return 'paper';
+  default:
+    primeUserChoice();
   }
 } 
+ 
+// if (userChoice === null) {
+//   return alert ('You aborted this game. To start new game just refresh the page.');
+// }
 
 function randomNumber (numMin, numMax) {
-  return Math.floor(Math.random() * (numMax - numMin) + numMin)
+  return Math.floor(Math.random() * (numMax - numMin) + numMin);
 }
 
 function game () {
   (primeUserChoice ());
   machineChoise = gameArray [randomNumber (0, 3)];
   alert (`Computer move is: ${machineChoise}`)
-  
-  if (UserChoice === 'rock' && machineChoise === 'scissors' || UserChoice === 'scissors' && machineChoise === 'paper' || UserChoice === 'paper' && machineChoise === 'rock') {
-    levay++;  
+
+  if (userChoice === 'rock' && machineChoise === 'rock' || userChoice === 'scissors' && machineChoise === 'scissors' || userChoice === 'paper' && machineChoise === 'paper') {
+    game();
+  } else if (userChoice === 'rock' && machineChoise === 'scissors' || userChoice === 'scissors' && machineChoise === 'paper' || userChoice === 'paper' && machineChoise === 'rock') {
+    userScore++;     
+    alert (`You won this round: Current count is ${userName}: ${userScore}: Computer ${computerScore}`);
   } else {  
-    levayDva++;
+    computerScore++;
+    alert (`Computer won this round: Current count is ${userName}: ${userScore}: Computer ${computerScore}`);
   }
 }
 
-for (levay; levay < 3;){ 
-  game();
-  alert (`Ball User ${levay}` + ` Ball PC ${levayDva}`);
+for (let i = 0; i < 1;) {   
+  if (userScore === 3) {
+    alert (`Congratulations. You won this game. Count - You: ${userScore} : Computer ${computerScore}`);
+    i++;
+  } else if ( computerScore === 3) {
+    alert (`Sorry. You lost this game. Count - You: ${userScore} : Computer ${computerScore}`);
+    i++;
+  } else {
+    game();
+  }
+}
+
+let newGame = confirm ('Do you want to start new game?');
+if (newGame) {
+  location.reload();
 }
